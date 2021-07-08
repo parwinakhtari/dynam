e4ff39dcea4369122352ad1e91630491e83a8119
Parwin Akhtari <68842515+parwinakhtari@users.noreply.github.com>
2021-07-08 12:35:24 +0530

    Create SubsetSum.java

---
+++ Basics/SubsetSum.java
@@ -0,0 +1,51 @@
+One variation: https://leetcode.com/problems/partition-equal-subset-sum/
+second variation: https://www.geeksforgeeks.org/subset-sum-problem-dp-25/
+Solution:
+
+    public boolean subsetSum(int input[], int total) {
+
+        boolean T[][] = new boolean[input.length + 1][total + 1];
+        for (int i = 0; i <= input.length; i++) {
+            T[i][0] = true;
+        }
+
+        for (int i = 1; i <= input.length; i++) {
+            for (int j = 1; j <= total; j++) {
+                if (j - input[i - 1] >= 0) {
+                    T[i][j] = T[i - 1][j] || T[i - 1][j - input[i - 1]];
+                } else {
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+        return T[input.length][total];
+
+    }
+
+    public boolean partition(int arr[]) {
+        int sum = 0;
+        for (int i = 0; i < arr.length; i++) {
+            sum += arr[i];
+        }
+
+        if (sum % 2 != 0) {
+            return false;
+        }
+        sum = sum / 2;
+        boolean[][] T = new boolean[arr.length + 1][sum + 1];
+
+        for (int i = 0; i <= arr.length; i++) {
+            T[i][0] = true;
+        }
+
+        for (int i = 1; i <= arr.length; i++) {
+            for (int j = 1; j <= sum; j++) {
+                if (j - arr[i - 1] >= 0) {
+                    T[i][j] = T[i - 1][j - arr[i - 1]] || T[i - 1][j];
+                } else {
+                    T[i][j] = T[i-1][j];
+                }
+            }
+        }
+        return T[arr.length][sum];
+    }
