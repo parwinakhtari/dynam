084b95da035719cbddcb63e5265b34982666c541
Parwin Akhtari <68842515+parwinakhtari@users.noreply.github.com>
2021-07-17 17:47:34 +0530

    Update editDistance.cpp

--- Intermediate/editDistance.cpp
+++ Intermediate/editDistance.cpp
@@ -2,24 +2,25 @@
  *
  * Given two strings how many minimum edits(update, delete or add) is needed to convert one string to another
  *
- * Time complexity is O(m*n)
- * Space complexity is O(m*n)
+ * Time complexity is O(l1*l2)
+ * Space complexity is O(l1*l2)
  *
  * References:
  * http://www.geeksforgeeks.org/dynamic-programming-set-5-edit-distance/
  * https://en.wikipedia.org/wiki/Edit_distance
+ * Video explanation https://www.youtube.com/watch?v=We3YDTzNXEk&list=PLrmLmBdmIlpsHaNTPP_jHHDx_os9ItYXr&index=6
  */
 // Utility function to find the minimum of three numbers
 int min(int x, int y, int z) { return min(min(x, y), z); }
  
-int editDistDP(string str1, string str2, int m, int n)
+int editDistDP(string str1, string str2, int l1, int l2)
 {
     // Create a table to store results of subproblems
-    int dp[m + 1][n + 1];
+    int dp[l1 + 1][l2 + 1];
  
     // Fill d[][] in bottom up manner
-    for (int i = 0; i <= m; i++) {
-        for (int j = 0; j <= n; j++) {
+    for (int i = 0; i <= l1; i++) {
+        for (int j = 0; j <= l2; j++) {
             // If first string is empty, only option is to
             // insert all characters of second string
             if (i == 0)
@@ -46,5 +47,5 @@ int editDistDP(string str1, string str2, int m, int n)
         }
     }
  
-    return dp[m][n];
+    return dp[l1][l2];
 }
